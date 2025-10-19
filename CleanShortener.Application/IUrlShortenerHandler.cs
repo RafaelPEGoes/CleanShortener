9f755f5379c3c87416951c82106f939f2528bef1
RafaelPEGoes <rafael.pelias@hotmail.com>
2025-10-19 19:34:43 -0300

    add project

---
+++ CleanShortener.Application/IUrlShortenerHandler.cs
@@ -0,0 +1,12 @@
+﻿using CleanShortener.Domain;
+
+namespace CleanShortener.Application;
+
+public interface IUrlShortenerHandler
+{
+    public Result<ShortUrlDto, ValidationErrors> CreateShortUrl(ShortUrlDto shortUrlRequest);
+
+    public ShortUrlDto GetShortenedUrl(ShortUrlDto shortUrlDto);
+
+    public ShortUrlDto GetShortenedUrlById(string shortUrlId);
+}
