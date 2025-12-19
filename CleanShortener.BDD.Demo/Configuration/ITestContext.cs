2b4f0be66d9aa8f386917a29a1e18cb87e1b1d0f
RafaelPEGoes <rafael.pelias@hotmail.com>
2025-12-19 01:08:11 -0300

    add new context abstraction, reduce the number of cross-cutting dependencies to be directly injected

---
+++ CleanShortener.BDD.Demo/Configuration/ITestContext.cs
@@ -0,0 +1,11 @@
+﻿using CleanShortener.BDD.Demo.Helpers;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+
+namespace CleanShortener.BDD.Demo.Configuration;
+
+public interface ITestContext
+{
+    public ServiceUrls ServiceUrls { get; }
+    public IHttpClientHelper HttpClient { get; }
+    public AccessTokenResponse? ServiceToken { get; set; }
+}
---
+++ CleanShortener.BDD.Demo/Configuration/TestContext.cs
@@ -0,0 +1,13 @@
+﻿using CleanShortener.BDD.Demo.Helpers;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.Extensions.Options;
+
+namespace CleanShortener.BDD.Demo.Configuration;
+
+public class TestContext(IOptions<ServiceUrls> serviceUrls, IHttpClientHelper httpClient) : ITestContext
+{
+    public ServiceUrls ServiceUrls { get; } = serviceUrls.Value;
+    public IHttpClientHelper HttpClient { get; } = httpClient;
+
+    public AccessTokenResponse? ServiceToken { get; set; }
+}
