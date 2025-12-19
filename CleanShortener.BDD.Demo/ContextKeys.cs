e662a2d489340a96dca14d01bdfee58523fdfedc
RafaelPEGoes <rafael.pelias@hotmail.com>
2025-12-19 02:45:16 -0300

    add new key

--- CleanShortener.BDD.Demo/ContextKeys.cs
+++ CleanShortener.BDD.Demo/ContextKeys.cs
@@ -1,7 +1,13 @@
 ﻿namespace CleanShortener.BDD.Demo;
 public static class ContextKeys
 {
+    #region Url
     public const string LongUrl = "LongUrl";
+    public const string OriginalUrl = "OriginalUrl";
+    #endregion
+
+    #region Response Keys
     public const string CreateShortUrlResponse = "CreateShortUrlResponse";
     public const string AccessTokenResponse = "AccessTokenResponse";
+    #endregion
 }
