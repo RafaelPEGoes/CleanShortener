52e0323d0d89e8748440966c36f5ec9e45d7bc58
RafaelPEGoes <rafael.pelias@hotmail.com>
2025-12-19 01:05:16 -0300

    add generic http client helper

---
+++ CleanShortener.BDD.Demo/Helpers/HttpClientHelper.cs
@@ -0,0 +1,23 @@
+﻿using CleanShortener.BDD.Demo.Configuration;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace CleanShortener.BDD.Demo.Helpers;
+
+public class HttpClientHelper : IHttpClientHelper
+{
+    private readonly ApiTestContext _apiContext;
+
+    public HttpClientHelper(ApiTestContext apiContext)
+    {
+        _apiContext = apiContext;
+    }
+    public async Task<TResponse> PostAsync<TRequest, TResponse>(string endpoint, TRequest request)
+    {
+        using var rawResponse = await _apiContext.HttpClient.PostAsJsonAsync(endpoint, request);
+
+        rawResponse.EnsureSuccessStatusCode();
+
+        return await rawResponse.Content.ReadFromJsonAsync<TResponse>()! ?? throw new InvalidOperationException($"HttpResponse from {endpoint} was null.");
+    }
+}
---
+++ CleanShortener.BDD.Demo/Helpers/IHttpClientHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace CleanShortener.BDD.Demo.Helpers;
+
+public interface IHttpClientHelper
+{
+    Task<TResponse> PostAsync<TRequest, TResponse>(string endpoint, TRequest request);
+}
